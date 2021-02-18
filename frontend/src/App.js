441eda99fb5a98cc51822f4e44ef5928407b4946
Rafal Jablonski <jablonski.rafal@gmail.com>
2021-02-18 22:37:36 +0100

    Add Home Screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,20 @@
-import './App.css';
+import { Container } from "react-bootstrap";
+
+import Header from './components/Header';
+import Footer from './components/Footer';
+
+import HomeScreen from "./screens/HomeScreen";
 
 function App() {
   return (
     <div>
-      My App
+        <Header />
+        <main className="py-3">
+            <Container>
+                <HomeScreen />
+            </Container>
+        </main>
+        <Footer />
     </div>
   );
 }
