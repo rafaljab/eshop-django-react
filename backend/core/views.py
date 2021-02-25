629039d83a39086d21ef79de0d2f7d9aa4d2041d
Rafal Jablonski <jablonski.rafal@gmail.com>
2021-02-25 19:52:06 +0100

    Add serializer and return data from DB

--- backend/core/views.py
+++ backend/core/views.py
@@ -1,9 +1,8 @@
-from django.shortcuts import render
-from django.http import JsonResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
-from .products import products
+from .models import Product
+from .serializer import ProductSerializer
 
 
 @api_view(['GET'])
@@ -23,15 +22,13 @@ def get_routes(request):
 
 @api_view(['GET'])
 def get_products(request):
-    return Response(products)
+    products = Product.objects.all()
+    serializer = ProductSerializer(products, many=True)
+    return Response(serializer.data)
 
 
 @api_view(['GET'])
 def get_product(request, pk):
-    product = None
-    for i in products:
-        if i['_id'] == pk:
-            product = i
-            break
-
-    return Response(product)
+    product = Product.objects.get(_id=pk)
+    serializer = ProductSerializer(product, many=False)
+    return Response(serializer.data)
